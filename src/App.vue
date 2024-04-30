a30241ad7920d0b10c8ae964fa3c66666fe3fad3
Bladesheng <keadr23@gmail.com>
2024-04-30 21:17:27 +0200

    use PrimeVue TabMenu for header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import type { MenuItem } from 'primevue/menuitem';
+import TabMenu from 'primevue/tabmenu';
+
+const items: MenuItem[] = [
+	{
+		label: 'Home',
+		route: { name: 'home' },
+	},
+	{
+		label: 'Movies',
+		route: { name: 'movies' },
+	},
+	{
+		label: 'TV Shows',
+		route: { name: 'tv' },
+	},
+	{
+		label: 'Settings',
+		route: { name: 'settings' },
+	},
+];
 </script>
 
 <template>
 	<header>
-		<div class="wrapper">
-			<nav class="flex gap-1">
-				<RouterLink :to="{ name: 'home' }">Home</RouterLink>
-				<RouterLink :to="{ name: 'movies' }">Movies</RouterLink>
-				<RouterLink :to="{ name: 'tv' }">TV Shows</RouterLink>
-				<RouterLink :to="{ name: 'settings' }">Settings</RouterLink>
-			</nav>
-		</div>
+		<TabMenu :model="items" class="m-4">
+			<template #item="{ item, props }">
+				<RouterLink v-bind="props.action" :to="item.route">
+					{{ item.label }}
+				</RouterLink>
+			</template>
+		</TabMenu>
 	</header>
 
 	<RouterView />
