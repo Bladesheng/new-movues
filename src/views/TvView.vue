42f9eb07e0b51a7960d399b47333adee79148efb
Bladesheng <keadr23@gmail.com>
2024-05-06 19:03:17 +0200

    move popularity slider to component

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -5,13 +5,12 @@ import { computed, onMounted, ref, watch } from 'vue';
 import PosterCard from '@/components/PosterCard.vue';
 import { useInfiniteScroll, useStorage } from '@vueuse/core';
 import ScaleTransitionGroup from '@/components/ScaleTransitionGroup.vue';
-import Slider from 'primevue/slider';
-import InputNumber from 'primevue/inputnumber';
 import SidebarLeft from '@/components/SidebarLeft.vue';
 import LoaderFooter from '@/components/LoaderFooter.vue';
 import SelectedGenres from '@/components/filters/SelectedGenres.vue';
 import SortOptions from '@/components/filters/SortOptions.vue';
 import type { SortOptionFull } from '@/types/tmdb';
+import PopularityMinSlider from '@/components/filters/PopularityMinSlider.vue';
 
 const bearerStore = useBearerStore();
 
@@ -116,49 +115,13 @@ async function checkIfMoreExist() {
 		await checkIfMoreExist();
 	}
 }
-
-function onWheel(e: WheelEvent) {
-	if (e.deltaY > 0) {
-		minPopularity.value -= 5;
-	} else {
-		minPopularity.value += 5;
-	}
-
-	if (minPopularity.value < 0) {
-		minPopularity.value = 0;
-	}
-
-	if (minPopularity.value > 100) {
-		minPopularity.value = 100;
-	}
-}
 </script>
 
 <template>
 	<main class="px-4">
 		<div class="flex">
 			<SidebarLeft>
-				<div class="flex flex-col gap-2">
-					<label for="popularity">
-						<strong>Minimal popularity</strong>
-					</label>
-
-					<div class="flex items-center gap-4">
-						<Slider
-							v-model="minPopularity"
-							:min="0"
-							:max="100"
-							@wheel.prevent="onWheel"
-							class="h-2 w-40"
-						/>
-
-						<InputNumber
-							v-model="minPopularity"
-							inputId="popularity"
-							inputClass="w-14 h-10 text-center"
-						/>
-					</div>
-				</div>
+				<PopularityMinSlider v-model="minPopularity" />
 
 				<SortOptions
 					v-model="sortBy"
