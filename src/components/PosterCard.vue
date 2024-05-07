883c8fdfae9e6936d523e7178717412e3fdaf03a
Bladesheng <keadr23@gmail.com>
2024-05-07 09:03:07 +0200

    fix displayed daysUntilAir

--- src/components/PosterCard.vue
+++ src/components/PosterCard.vue
@@ -35,15 +35,16 @@ const daysUntilAir = computed(() => {
 
 	const daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24)) + 1;
 
-	switch (daysLeft) {
-		case -1:
-			return 'Yesterday';
-		case 0:
-			return 'Today';
-		case 1:
-			return 'Tomorrow';
-		default:
-			return `in ${daysLeft} days`;
+	if (daysLeft <= -2) {
+		return `${daysLeft * -1} days ago`;
+	} else if (daysLeft === -1) {
+		return 'Yesterday';
+	} else if (daysLeft === 0) {
+		return 'Today';
+	} else if (daysLeft === 1) {
+		return 'Tomorrow';
+	} else {
+		return `in ${daysLeft} days`;
 	}
 });
 
