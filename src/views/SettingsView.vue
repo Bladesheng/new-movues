881fec1b3833cda42953add4ae43440254b77ae8
Bladesheng <keadr23@gmail.com>
2024-05-13 18:47:46 +0200

    change favicon

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -6,4 +6,14 @@ import TmdbAccessTokenSettings from '@/components/TmdbAccessTokenSettings.vue';
 	<main class="px-4">
 		<TmdbAccessTokenSettings />
 	</main>
+
+	<footer class="flex justify-center">
+		<small>
+			Icons made from
+			<a class="underline" href="https://www.onlinewebfonts.com/icon" target="_blank">
+				svg icons
+			</a>
+			is licensed by CC BY 4.0
+		</small>
+	</footer>
 </template>
