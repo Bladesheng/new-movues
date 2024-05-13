2cf36d5f4f0045ff7f16251f905cf683b907f62f
Bladesheng <keadr23@gmail.com>
2024-05-13 19:20:01 +0200

    add github link

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -7,7 +7,7 @@ import TmdbAccessTokenSettings from '@/components/TmdbAccessTokenSettings.vue';
 		<TmdbAccessTokenSettings />
 	</main>
 
-	<footer class="flex justify-center">
+	<footer class="flex flex-col items-center gap-2 text-gray-600">
 		<small>
 			Icons made from
 			<a class="underline" href="https://www.onlinewebfonts.com/icon" target="_blank">
@@ -15,5 +15,28 @@ import TmdbAccessTokenSettings from '@/components/TmdbAccessTokenSettings.vue';
 			</a>
 			is licensed by CC BY 4.0
 		</small>
+
+		<small class="flex items-center gap-1.5">
+			Made by
+			<a
+				href="https://github.com/Bladesheng/new-movues"
+				target="_blank"
+				class="inline-flex items-center gap-1 text-black"
+			>
+				<img src="/github-mark.svg" alt="github-mark" class="inline h-5" />
+				Bladesheng
+			</a>
+		</small>
 	</footer>
 </template>
+
+<style scoped>
+img {
+	transition: transform 1s;
+}
+
+a:hover > img {
+	transition: transform 2s;
+	transform: rotate(720deg);
+}
+</style>
