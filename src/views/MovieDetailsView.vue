637581843331e168bc5410e44f9809023f6bd6f1
Bladesheng <keadr23@gmail.com>
2024-06-30 09:33:03 +0200

    add tmdb and csfd details to movie details view

--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -2,9 +2,13 @@
 import { useRoute } from 'vue-router';
 import { useTmdbAccessToken } from '@/stores/tmdbAccessToken';
 import { type AppendToResponse, type MovieDetails, TMDB } from 'tmdb-ts';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { CSFDMovie } from 'node-csfd-api/interfaces/movie.interface';
 import { getCsfdMovie } from '@/api/csfdApi';
+import CsfdDetails from '@/components/CsfdDetails.vue';
+import Skeleton from 'primevue/skeleton';
+import LoadingSpinner from '@/assets/LoadingSpinner.vue';
+import TmdbDetails from '@/components/TmdbDetails.vue';
 
 const route = useRoute();
 
@@ -24,6 +28,17 @@ const csfdMovie = ref<CSFDMovie>();
 const movieId = parseInt(route.params.id as string);
 
 getDetails();
+
+const runtimeText = computed(() => {
+	if (tmdbRes.value === undefined) {
+		return undefined;
+	}
+
+	const hours = Math.floor(tmdbRes.value.runtime / 60);
+	const minutes = Math.floor(tmdbRes.value.runtime % 60);
+
+	return `${hours} h ${minutes} min`;
+});
 
 async function getDetails() {
 	const res = await tmdb.movies.details(
@@ -47,7 +62,43 @@ async function getDetails() {
 </script>
 
 <template>
-	<a :href="`https://www.themoviedb.org/movie/${route.params.id}`"> movie details </a>
+	<div class="m-4 flex grow">
+		<template v-if="tmdbRes === undefined">
+			<div class="flex grow items-center justify-center">
+				<LoadingSpinner class="h-20 w-20 text-emerald-500" />
+			</div>
+		</template>
+
+		<template v-else>
+			<TmdbDetails
+				:name="tmdbRes.title"
+				:releaseDate="new Date(tmdbRes.release_date)"
+				:posterPath="tmdbRes.poster_path!"
+				:genres="tmdbRes.genres"
+				:rating="tmdbRes.vote_average"
+				:tagline="tmdbRes.tagline"
+				:overview="tmdbRes.overview"
+				:createdBy="tmdbRes.credits.crew[0]"
+				:keywords="tmdbRes.keywords.keywords"
+				:runtimeText="runtimeText"
+				:cast="tmdbRes.credits.cast.slice(0, 10)"
+				:videos="tmdbRes.videos.results.filter((video) => video.site === 'YouTube')"
+				:tmdbLink="`https://www.themoviedb.org/movie/${route.params.id}`"
+				:imdbId="tmdbRes.external_ids.imdb_id"
+				:images="tmdbRes.images"
+			>
+				<template #csfdCard>
+					<template v-if="csfdMovie === undefined">
+						<Skeleton width="16rem" height="25rem" />
+					</template>
+
+					<template v-else>
+						<CsfdDetails :csfdMovie="csfdMovie" />
+					</template>
+				</template>
+			</TmdbDetails>
+		</template>
+	</div>
 </template>
 
 <style scoped></style>
