f21457d9aa7a8b846c2dcc017caa431d0c6d55c9
Bladesheng <keadr23@gmail.com>
2024-08-26 22:30:46 +0200

    ci: fix ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173',
+		baseURL: 'http://localhost:5179',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -64,8 +64,10 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'npm run build-only && npm run preview',
-		url: 'http://localhost:5173',
+		// command: 'npm run build-only && npm run preview',
+		command: 'npm run dev',
+		// url: 'http://localhost:5173',
+		port: 5179,
 		reuseExistingServer: !process.env.CI,
 	},
 });
